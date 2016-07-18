1cac689e703e7e8e53d148bbda6b14f9edfc30d5
Huy Dang Lê-Ngô <huy-dang.le-ngo@polymtl.ca>
2016-07-18 16:00:25 -0400

    Initial commit with Hello World

---
+++ README.md
@@ -0,0 +1,15 @@
+# agate
+
+This repository contains the source for *agate*, an Electron-based music player.
+
+## Deployment
+
+1. Install the required packages using `npm install`
+2. Start the application using `npm start`.
+
+## Commands
+
+* `npm install`: Installs the *NPM* dependencies.
+* `npm run clean`: Removes the `dist` directory.
+* `npm run build`: Bundles all of the application in the `dist` directory.
+* `npm start`: Starts the Electron-based application located in the `dist` folder.
---
+++ src/index.js
@@ -0,0 +1,26 @@
+import { app, BrowserWindow } from 'electron';
+import { join } from 'path';
+
+let mainWindow;
+
+const createWindow = () => {
+    mainWindow = new BrowserWindow({ width: 800, height: 600 });
+    mainWindow.loadURL(`file://${ join(__dirname, 'index.html') }`);
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    });
+};
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (mainWindow === null) {
+        createWindow();
+    }
+});
